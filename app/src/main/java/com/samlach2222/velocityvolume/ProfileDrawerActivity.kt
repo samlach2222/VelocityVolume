d947c0ca81e0cf612af66d81eb9ebf6c582fae93
samlach2222 <lachaudsamuel@protonmail.com>
2022-06-20 16:23:08 +0200

    Add ability to delete profiles

--- app/src/main/java/com/samlach2222/velocityvolume/ProfileDrawerActivity.kt
+++ app/src/main/java/com/samlach2222/velocityvolume/ProfileDrawerActivity.kt
@@ -1,13 +1,15 @@
 package com.samlach2222.velocityvolume
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.core.view.GravityCompat
+import androidx.core.view.get
 import androidx.core.view.size
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -15,11 +17,17 @@ import androidx.navigation.ui.*
 import com.google.android.material.navigation.NavigationView
 import com.samlach2222.velocityvolume.databinding.ActivityProfileDrawerBinding
 
+
 class ProfileDrawerActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityProfileDrawerBinding
     private lateinit var drawerLayout: DrawerLayout
+    private var profileNameTable = ArrayList<String>()
+
+    // id
+    private var currentFreeId = 0
+    private var listIdMenu : HashMap<String, Int> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +56,7 @@ class ProfileDrawerActivity : AppCompatActivity() {
                     navController.navigate(R.id.nav_settings)
                 }
                 R.id.add_new_profile -> addProfile()
+                R.id.delete_profiles -> deleteProfiles()
                 else -> {
                     val bundle = bundleOf("id" to menuItem.toString())
                     navController.navigate(R.id.nav_volumemanager, bundle)
@@ -62,9 +71,51 @@ class ProfileDrawerActivity : AppCompatActivity() {
             true
         }
 
-        if(navView.menu.size == 2) { // if there is no profile created = First launch of app
+        if(navView.menu.size == 3) { // if there is no profile created = First launch of app
             navController.navigate(R.id.nav_homepage)
         }
+    }
+
+    private fun deleteProfiles() {
+        // TODO : Redirect first available profile when we delete current profile
+
+        val dialogBuilder = AlertDialog.Builder(this@ProfileDrawerActivity)
+        dialogBuilder.setTitle("Choose profiles")
+
+        var profileNameToDelete = ArrayList<String>()
+
+        val arr: Array<String?> = arrayOfNulls<String>(profileNameTable.size)
+        for (i in 0 until profileNameTable.size) {
+            arr[i] = profileNameTable[i]
+        }
+
+        dialogBuilder.setMultiChoiceItems(arr, null) { _, which, isChecked ->
+            // The user checked or unchecked a box
+            if(isChecked){
+                profileNameToDelete.add(profileNameTable[which])
+            }
+            else{
+                profileNameToDelete.remove(profileNameTable[which])
+            }
+        }
+        dialogBuilder.setPositiveButton("Delete",
+            DialogInterface.OnClickListener { _, _ ->
+                // The user clicked Delete
+                for(i in 0 until profileNameToDelete.size) {
+                    profileNameTable.remove(profileNameToDelete[i])
+                    // delete link
+                    val idToDelete = listIdMenu[profileNameToDelete[i]]
+                    val menu = findViewById<NavigationView>(R.id.nav_view).menu
+                    if (idToDelete != null) {
+                        menu.removeItem(idToDelete)
+                    }
+                }
+            })
+        dialogBuilder.setNegativeButton("Cancel", null)
+
+        // Create and show the alert dialog
+        val dialog: AlertDialog = dialogBuilder.create()
+        dialog.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -75,10 +126,11 @@ class ProfileDrawerActivity : AppCompatActivity() {
     fun addProfile() {
         val editTextField = EditText(this@ProfileDrawerActivity)
         editTextField.isSingleLine = true
+        editTextField.gravity = Gravity.CENTER_HORIZONTAL
 
         val dialog: AlertDialog = AlertDialog.Builder(this@ProfileDrawerActivity)
             .setTitle("Name of the new profile")
-            .setMessage("")  // toast or message ?
+            .setMessage("")
             .setView(editTextField)
             .setPositiveButton("OK", null)
             .setNegativeButton("Cancel", null)
@@ -90,25 +142,30 @@ class ProfileDrawerActivity : AppCompatActivity() {
                 val enteredText = editTextField.text.toString().trim()
                 val enteredTextLowercase = enteredText.lowercase()
 
-                // TODO : Check if a profile with the same name doesn't already exist
-                val profileAlreadyExist: Boolean = enteredTextLowercase == "profile 1"
-                    || enteredTextLowercase == "profile 2" || enteredTextLowercase == "profile 3"
+                // Get if the profileName already exists
+                var profileAlreadyExist = false
+                if(profileNameTable.contains(enteredTextLowercase)) {
+                    profileAlreadyExist = true
+                }
+
+
                 if (!profileAlreadyExist) {
                     dialog.setMessage("")
                     val menu = findViewById<NavigationView>(R.id.nav_view).menu
 
-                    val newMenuItem = menu.add(R.id.group_profiles, Menu.NONE, 100, enteredText)
+                    val newMenuItem = menu.add(R.id.group_profiles, currentFreeId, 100, enteredText)
+                    listIdMenu[enteredText] = currentFreeId // add new element in the list
                     newMenuItem.setIcon(R.drawable.ic_menu_person)
+                    currentFreeId++
 
                     //Redirect to the newly created profile
                     val bundle = bundleOf("id" to newMenuItem.toString())
                     val navController = findNavController(R.id.nav_host_fragment_content_profile_drawer)
                     navController.navigate(R.id.nav_volumemanager, bundle)
                     dialog.dismiss()
+                    profileNameTable.add(enteredTextLowercase)
                 } else {
-                    // toast or message ?
                     dialog.setMessage("A profile named " + editTextField.text.toString() + " already exist")
-                    Toast.makeText(this@ProfileDrawerActivity,"A profile named " + editTextField.text.toString() + " already exist", Toast.LENGTH_SHORT).show()
                 }
             }
         }
