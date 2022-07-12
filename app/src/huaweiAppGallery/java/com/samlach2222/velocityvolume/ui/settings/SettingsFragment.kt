af6912d2773db6548887ae5307c8d449371cc233
samlach2222 <lachaudsamuel@protonmail.com>
2022-07-12 16:16:28 +0200

    Verify if HMS available for Update in huawei Flavor

--- app/src/huaweiAppGallery/java/com/samlach2222/velocityvolume/ui/settings/SettingsFragment.kt
+++ app/src/huaweiAppGallery/java/com/samlach2222/velocityvolume/ui/settings/SettingsFragment.kt
@@ -1,12 +1,18 @@
 package com.samlach2222.velocityvolume.ui.settings
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.huawei.hms.api.ConnectionResult
+import com.huawei.hms.api.HuaweiApiAvailability
 import com.huawei.hms.jos.AppUpdateClient
 import com.huawei.hms.jos.JosApps
 import com.huawei.updatesdk.service.appmgr.bean.ApkUpgradeInfo
 import com.huawei.updatesdk.service.otaupdate.CheckUpdateCallBack
 import com.huawei.updatesdk.service.otaupdate.UpdateKey
+
 
 /**
  * The Settings fragment class manages the interactivity of the Settings ui
@@ -14,8 +20,25 @@ import com.huawei.updatesdk.service.otaupdate.UpdateKey
 class SettingsFragment : SettingsFragmentAbstract() {
     override fun updateApp() {
         // Check Update
-        val client = JosApps.getAppUpdateClient(context)
-        client.checkAppUpdate(context, UpdateCallBack(requireContext()))
+        val client = JosApps.getAppUpdateClient(requireContext())
+
+        if(isHmsAvailable(requireContext())){ // Check if Huawei Mobile Services available
+            client.checkAppUpdate(requireContext(), UpdateCallBack(requireContext()))
+        }
+        else {
+            Toast.makeText(requireContext(), "HMS not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isHmsAvailable(context: Context?): Boolean {
+        var isAvailable = false
+        if (null != context) {
+            val result =
+                HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context)
+            isAvailable = ConnectionResult.SUCCESS == result
+        }
+        Log.i(TAG, "isHmsAvailable: $isAvailable")
+        return isAvailable
     }
 }
 
